c8a0317e8753429d46074e4102f1d21775fd2e4a
Eino <eino.juhani.oltedal@cern.ch>
2015-07-06 17:20:32 +0200

    started working on the warning system for blms

--- charm_shift_tool.py
+++ charm_shift_tool.py
@@ -1,1 +1,21 @@
+from data_retriver import Bpm 
 
+bpm_url = "https://ps-irrad.web.cern.ch/irrad/bpm.php?bpmid=BPM_0"
+
+deviation = .3
+
+def check_bpm():
+  # BPMs are numbered 1 through 4
+  # index 0 contains refrence data,
+  # and the last index contains the most recent sampling
+  b = Bpm()
+  for i in range(1,5):
+    data = b.dl_bpm_data(bpm_url+str(i))
+    xdata = b.extract_xy_data(data['X'])
+    ydata = b.extract_xy_data(data['Y'])
+    for j in  ('intensity','fwhm'):
+      if ((xdata[-1][j] > (1+deviation)*xdata[0][j]) or (xdata[-1][j] < (1-deviation)*xdata[0][j])):
+        print("Large deviation in the " + j + " of BPM" + str(i))
+        print(" reference " + j + " " + str(xdata[0][j]))
+        print(" current " + j + " " + str(xdata[0][j]))
+check_bpm()
--- data_retriver.py
+++ data_retriver.py
@@ -4,63 +4,57 @@ import json
 from pprint import pprint
 import numpy as np
 
-bpm_url = "https://ps-irrad.web.cern.ch/irrad/bpm.php?bpmid=BPM_0"
 
 def fwhm(sigma):
   return round(sigma*2.355,2)
 
-def integralMean(data):
-  weight = 0.0
-  weightedSum = 0.0
-  for arr in data:
-    weight += arr[1]
-    weightedSum += arr[1]*arr[0];
-  if(weight == 0):
-    return (0)
-  return round(weightedSum/weight,2)
+class Bpm:
 
-def sigma(data,mean):
-  W = 0.0
-  Q = 0.0
-  for arr in data:
-    W += arr[1]
-    Q += arr[1]*(arr[0]-mean)*(arr[0]-mean)
-  s = np.sqrt(Q/W);
-  return round(s,2)
+  def integralMean(self,data):
+    weight = 0.0
+    weightedSum = 0.0
+    for arr in data:
+      weight += arr[1]
+      weightedSum += arr[1]*arr[0];
+    if(weight == 0):
+      return (0)
+    return round(weightedSum/weight,2)
 
-# Returns dictionary that contains reference data aswell as the last 
-# 4 time stamped samplings
-def dl_bpm_data(url):
+  def sigma(self, data,mean):
+    W = 0.0
+    Q = 0.0
+    for arr in data:
+      W += arr[1]
+      Q += arr[1]*(arr[0]-mean)*(arr[0]-mean)
+    s = np.sqrt(Q/W);
+    return round(s,2)
 
-  html = request.urlopen(url).read().decode("utf8")
-  json_table = re.findall(r"startData = (.*);",html)[0]
-  data_dict = json.loads(json_table)
+  # Returns dictionary that contains reference data aswell as the last 
+  # 4 time stamped samplings
+  def dl_bpm_data(self, url):
 
-  return data_dict
+    html = request.urlopen(url).read().decode("utf8")
+    json_table = re.findall(r"startData = (.*);",html)[0]
+    data_dict = json.loads(json_table)
 
-def extract_xy_data(data_dict):
-    data_arr = []
-    
-    for i in data_dict:
-      data = {}
-      data['title'] = i['title']
-      data['mean']  = integralMean(i['data'])
-      data['sigma'] = sigma(i['data'], data['mean'])
-      data['fwhm'] = fwhm(data['sigma'])
-      int_arr = []
-      for j in i['data']:
-        int_arr.append(j[1])
-      data['intensity'] = max(int_arr)
-      data_arr.append(data)
+    return data_dict
 
-    return data_arr
+  #
+  def extract_xy_data(self, data_dict):
+      data_arr = []
+      
+      for i in data_dict:
+        data = {}
+        data['title'] = i['title']
+        data['mean']  = self.integralMean(i['data'])
+        data['sigma'] = self.sigma(i['data'], data['mean'])
+        data['fwhm'] = fwhm(data['sigma'])
+        int_arr = []
+        for j in i['data']:
+          int_arr.append(j[1])
+        data['intensity'] = max(int_arr)
+        data_arr.append(data)
 
-def check_blm():
-  # BPMs are numbered 1 through 4
-  for i in range(1,5):
-    data = dl_bpm_data(bpm_url+str(i))
-    xdata = extract_xy_data(data['X'])
-    ydata = extract_xy_data(data['Y'])
-    print(xdata[-1]['intensity'])
+      return data_arr
+  
 
-check_blm()
