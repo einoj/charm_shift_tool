bc32413f7c627580fd06a692143e8d48cc0ca7c7
Eino <eino.juhani.oltedal@cern.ch>
2015-07-14 15:29:31 +0200

    Started using .ui files generated by QTdesigner

---
+++ cst.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'cst.ui'
+#
+# Created by: PyQt5 UI code generator 5.4.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setGeometry(QtCore.QRect(540, 0, 261, 551))
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 259, 549))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.spinBox = QtWidgets.QSpinBox(self.centralwidget)
+        self.spinBox.setGeometry(QtCore.QRect(320, 10, 181, 31))
+        self.spinBox.setObjectName("spinBox")
+        self.spinBox_2 = QtWidgets.QSpinBox(self.centralwidget)
+        self.spinBox_2.setGeometry(QtCore.QRect(320, 60, 181, 31))
+        self.spinBox_2.setObjectName("spinBox_2")
+        self.spinBox_3 = QtWidgets.QSpinBox(self.centralwidget)
+        self.spinBox_3.setGeometry(QtCore.QRect(320, 110, 181, 31))
+        self.spinBox_3.setObjectName("spinBox_3")
+        self.spinBox_4 = QtWidgets.QSpinBox(self.centralwidget)
+        self.spinBox_4.setGeometry(QtCore.QRect(320, 160, 181, 31))
+        self.spinBox_4.setObjectName("spinBox_4")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 15, 301, 21))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(10, 60, 301, 31))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(10, 110, 301, 31))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setGeometry(QtCore.QRect(10, 160, 281, 31))
+        self.label_4.setObjectName("label_4")
+        self.spinBox_5 = QtWidgets.QSpinBox(self.centralwidget)
+        self.spinBox_5.setGeometry(QtCore.QRect(320, 210, 181, 31))
+        self.spinBox_5.setObjectName("spinBox_5")
+        self.spinBox_6 = QtWidgets.QSpinBox(self.centralwidget)
+        self.spinBox_6.setGeometry(QtCore.QRect(320, 260, 181, 31))
+        self.spinBox_6.setObjectName("spinBox_6")
+        self.spinBox_7 = QtWidgets.QSpinBox(self.centralwidget)
+        self.spinBox_7.setGeometry(QtCore.QRect(320, 310, 181, 31))
+        self.spinBox_7.setObjectName("spinBox_7")
+        self.label_5 = QtWidgets.QLabel(self.centralwidget)
+        self.label_5.setGeometry(QtCore.QRect(10, 210, 291, 21))
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.label_6.setGeometry(QtCore.QRect(10, 260, 281, 31))
+        self.label_6.setObjectName("label_6")
+        self.label_7 = QtWidgets.QLabel(self.centralwidget)
+        self.label_7.setGeometry(QtCore.QRect(10, 310, 281, 31))
+        self.label_7.setObjectName("label_7")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label.setText(_translate("MainWindow", "Down time before users are alerted [min]"))
+        self.label_2.setText(_translate("MainWindow", "Deviation before users are alerted [%]"))
+        self.label_3.setText(_translate("MainWindow", "SEC reference [e11]"))
+        self.label_4.setText(_translate("MainWindow", "MWPC vertical FWHM refrence"))
+        self.label_5.setText(_translate("MainWindow", "MWPC vertical Centre reference [mm]"))
+        self.label_6.setText(_translate("MainWindow", "MWPC horizontal FWHM reference"))
+        self.label_7.setText(_translate("MainWindow", "MWPC horizontal Centre  reference [mm]"))
+
--- cst_gui.py
+++ cst_gui.py
@@ -1,14 +1,26 @@
 import sys
-from PyQt5.QtCore import QUrl
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtQuick import QQuickView
+#from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from cst import Ui_MainWindow
+
+class Window(QMainWindow, Ui_MainWindow):
+  def __init__(self, parent = None):
+
+    QMainWindow.__init__(self, parent)
+
+    self.setupUi(self)
 
 if __name__ == '__main__':
   cst = QApplication(sys.argv)
-  appLabel = QQuickView()
-  appLabel.setSource(QUrl('cst.qml'))
+  window = Window()
 
-  appLabel.show()
+  window.show()
+  #appLabel = QQuickView()
+  #appLabel.setSource(QUrl('cst.qml'))
 
-  cst.exec_()
-  sys.exit()
+  #appLabel.show()
+
+  #cst.exec_()
+  sys.exit(cst.exec_())
+
