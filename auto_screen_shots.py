fdfc4eefa19d39c92c1c0da49537b6a1d81ac860
Eino <eino.juhani.oltedal@cern.ch>
2015-07-27 23:51:05 +0200

    auto generating detector screenshots every 15 min

---
+++ auto_screen_shots.py
@@ -0,0 +1,66 @@
+from archive.database_call_v0 import lgdb_tools
+from data_retriver import Timber_detectors
+from datetime import datetime 
+from matplotlib.dates import date2num
+from matplotlib.pyplot import plot_date, show, title, grid, legend, xlabel, ylabel, savefig
+from time import sleep
+import subprocess
+import zipfile
+
+def sec_screens():
+    variable_name = 'MSC01.ZT8.107:COUNTS'
+    filename = 'sec_screen_data'
+    timeformat = '%Y-%m-%d %H:%M:%S.%f'
+    headers = ['Time [local]','Counts']
+    deltahours = 3.00
+
+    sec = Timber_detectors()
+
+    sec.fetch_from_timber(variable_name, filename, deltahours)
+
+    f = open('data/'+filename+'.CSV', 'r')
+    for i in range(7):
+      f.readline() # read first 7 header lines
+    
+    times = []
+    counts = []
+    for line in f:
+      l = line.split(',')
+      times.append(datetime.strptime(l[0], timeformat))
+      counts.append(l[1])
+    
+    dates = date2num(times)
+    plot_date(dates,counts,'r-',label=variable_name)
+    title(str(times[0])[0:19] + ' to ' + str(times[-1])[0:19]) # [0:19] removes the microseconds to avoid too long title
+    legend([variable_name])
+    grid(True)
+    xlabel('LOCAL_TIME')
+    ylabel('count')
+
+    savefig('//cern.ch/dfs/Websites/t/test-charmShiftTool/screens/sec.png', dpi=220)
+
+def web_screens():
+  p = subprocess.Popen('./phantomjs.exe ./user_tools/auto_logger.js', stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+if __name__ == "__main__":
+  directory = '//cern.ch/dfs/Websites/t/test-charmShiftTool/screens/'
+  while True:
+    sec_screens()
+    web_screens()
+    print('Creating zip file')
+    zf = zipfile.ZipFile(directory+'all_screens.zip', mode = 'w')
+    try:
+      print('Adding bpm1.png')
+      zf.write(directory+'bpm1.png',arcname='bpm1.png')
+      print('Adding bpm_all.png')
+      zf.write(directory+'bpm_all.png',arcname='bpm_all.png')
+      print('Adding sec.png')
+      zf.write(directory+'sec.png',arcname='sec.png')
+      print('Adding mwpc.png')
+      zf.write(directory+'mwpc.png',arcname='mwpc.png')
+      print('Adding op.png')
+      zf.write(directory+'op.png',arcname='op.png')
+    finally:
+      print('closing')
+      zf.close()
+    sleep(900) #sleep 900 seconds aka 15 min
--- user_tools/auto_logger.js
+++ user_tools/auto_logger.js
@@ -1,6 +1,6 @@
 var page = require('webpage').create(), loadInProgress = false, fs = require('fs');
 var htmlFiles = ['http://ps-irrad.web.cern.ch/irrad/intensityAll.php','http://ps-irrad.web.cern.ch/irrad/bpm.php?bpmid=BPM_01','http://thornton.web.cern.ch/charm_mwpc.html','https://op-webtools.web.cern.ch/op-webtools/vistar/vistars.php?usr=CPS'];
-var images = ['bpm_all', 'bpm1', 'mwpc', 'op'];
+var images = ['//cern.ch/dfs/Websites/t/test-charmShiftTool/screens/bpm_all', '//cern.ch/dfs/Websites/t/test-charmShiftTool/screens/bpm1', '//cern.ch/dfs/Websites/t/test-charmShiftTool/screens/mwpc', '//cern.ch/dfs/Websites/t/test-charmShiftTool/screens/op'];
  
 console.log('Number of Html Files: ' + htmlFiles.length);
 var width = 1920;
@@ -30,7 +30,7 @@ page.onLoadFinished = function() {
     window.setTimeout(function () {
       loadInProgress = false;
       console.log(images[url_index] + ' load finished');
-      page.render('images/' + images[url_index]+ ".png");
+      page.render(images[url_index]+ ".png");
       url_index++;
     }, 5000); // Timeout to allow sufficient time for rendring 
 }
