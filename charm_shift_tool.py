37256601b181b008381e0b1b4b823df7c92a3076
Eino <eino.juhani.oltedal@cern.ch>
2015-08-14 15:28:52 +0200

    Removed the roundTime function

--- charm_shift_tool.py
+++ charm_shift_tool.py
@@ -12,20 +12,6 @@ calibration = {
     'SEC1': 2.2E7
 }
 
-def roundTime(dt=None, roundTo=60):
-    '''
-    Round a datetime object to any time laps in seconds
-    dt : datetime.datetime object, default now.
-    roundTo : Closest number of seconds to round to, default 1 minute.
-    Author: Thierry Husson 2012 - Use it as you want but don't blame me.
-    '''
-
-    if dt == None: dt = datetime.now()
-    seconds = (dt - dt.min).seconds
-    # // is a floor division, not a comment on following line:
-    rounding = (seconds+roundTo/2) // roundTo * roundTo
-    return dt + timedelta(0,rounding-seconds,-dt.microsecond)
-
 def bpm_msg(data, axis):
   # BPMs are numbered 1 through 4
   # index 0 contains refrence data,
