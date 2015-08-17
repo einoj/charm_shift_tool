64ba6ce559a7541d094a184959d51152c07c6e86
Eino Juhani Oltedal <eino.juhani.oltedal@cern.ch>
2015-08-17 15:49:38 +0200

    moved cgi-bin to tool

--- www/install_www.cmd
+++ www/install_www.cmd
@@ -1,9 +1,11 @@
 @echo off
 set install_location=%1
-::copy cgi-bin\* \\cern.ch\dfs\Websites\t\test-charmShiftTool\admin\
-::copy admin\* \\cern.ch\dfs\Websites\t\test-charmShiftTool\admin\
 if not exist %install_location% (
-    echo 'derp'
-    exit -1
+    echo 'Please specify a valid install path as an argument'
+    exit /B
 )
-    echo 'herp'
+
+::copy cgi-bin\* %install_location%\cgi-bin 
+::copy admin\* %install_location%\admin
+
+ls %install_location%\admin
