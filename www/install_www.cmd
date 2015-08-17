0859a426ca3bef6638a9ca2a173225ea11ce8b30
Eino Juhani Oltedal <eino.juhani.oltedal@cern.ch>
2015-08-17 16:48:50 +0200

    remove database_ctrl.py from www/admin and www/tool beacuse they are just duplicates of the file in the main dir

--- www/install_www.cmd
+++ www/install_www.cmd
@@ -1,11 +1,13 @@
 @echo off
-set install_location=%1
+set install_location=g:\Websites\c\charmshifttool
 if not exist %install_location% (
     echo 'Please specify a valid install path as an argument'
     exit /B
 )
 
 copy tool\* %install_location%
+copy ..\database_ctrl.py %install_location%
 mkdir %install_location%\admin
 copy admin\* %install_location%\admin\
+copy ..\database_ctrl.py %install_location%\admin\
 
