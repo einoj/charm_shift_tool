02176028c30990bfb7ac6e69a89b8f1e73c166fa
Eino Juhani Oltedal <e.j.oltedal@fys.uio.no>
2015-08-21 17:53:11 +0200

    comments to install script

--- charmshifttool_install.sh
+++ charmshifttool_install.sh
@@ -1,5 +1,10 @@
+# This script will install the charm_shift_tool to a CERN
+# openstack server. It is assumed that the server 
+# is using a CC7 Extra x86_64# image
+
 # Update all programs then install git
 yum update
 yum install git
 
+#install python3
 # Create a new charmshift user
